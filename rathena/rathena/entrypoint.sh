69579c7302e80fe9ebce80e20c76114b15bc0973
iamlexivale <iamlexivale@gmail.com>
2023-06-16 21:55:04 +0700

    Update entrypoint.sh

--- rathena/rathena/entrypoint.sh
+++ rathena/rathena/entrypoint.sh
@@ -1,12 +1,2 @@
 #!/bin/ash
 cd /home/container
-
-echo "Installation started..."
-git clone https://github.com/rathena/rathena.git .
-./configure --enable-packetver=20220406
-make clean
-make server
-chmod a+x login-server && chmod a+x char-server && chmod a+x map-server
-
-echo "Installation finished..."
-exit
