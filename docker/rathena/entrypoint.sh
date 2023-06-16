a713545fed6af371e518b714354c1d776fc9b13c
iamlexivale <iamlexivale@gmail.com>
2023-06-16 20:20:37 +0700

    qwdq

--- docker/rathena/entrypoint.sh
+++ docker/rathena/entrypoint.sh
@@ -1,9 +1,2 @@
 #!/bin/ash
 cd /home/container
-
-echo "Initalizing Docker container..."
-git clone https://github.com/rathena/rathena.git .
-./configure --enable-packetver=${PACKETVER}
-make clean
-make server
-chmod a+x login-server && chmod a+x char-server && chmod a+x map-server 
